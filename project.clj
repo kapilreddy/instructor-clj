908e8e149505bbdedb7efd7eb2b94baf4d877258
Kapil Reddy <reddy.kapil@gmail.com>
2024-03-25 19:57:35 +0530

    Add initial working code

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject nanoGPT "0.0.1-alpha.1"
+  :author "Kapil Reddy <https://www.kapilreddy.me>"
+  :description "A Clojure implementation of Python library https://github.com/jxnl/instructor"
+
+  :dependencies
+  [[org.clojure/clojure "1.11.2"]
+   [cheshire "5.12.0"]
+   [metosin/malli "0.15.0"]
+   [stencil "0.5.0"]
+   [http-kit "2.7.0"]])
---
+++ src/instructor-clj/core.clj
@@ -0,0 +1,60 @@
+(ns instructor-clj.core
+  (:require [org.httpkit.client :as http]
+            [stencil.core :as sc]
+            [malli.json-schema :as json-schema]
+            [cheshire.core :as cc]))
+
+
+(defn schema->system-prompt
+  "Convert a malli schema into JSON schema and generate a system prompt for responses"
+  [schema]
+  (sc/render-string "As a genius expert, your task is to understand the content and provide
+                the parsed objects in json that match the following json_schema:\n
+
+                {{schema}}
+
+                Make sure to return an instance of the JSON, not the schema itselfHi there, {{name}}."
+                    {:schema (json-schema/transform schema)}))
+
+
+(defn llm->response
+  "response-schema - Valid malli schema
+
+  The function tries to destructure and get the actual response.
+  @TODO Add ability to plugin different LLMs
+  @TODO Getting response is brittle and not extensible for different LLMs"
+  [prompt response-schema & {:keys [api-key]}]
+  (let [api-url "https://api.openai.com/v1/chat/completions"
+        headers {"Authorization" (str "Bearer " api-key)
+                 "Content-Type" "application/json"}
+        body (cc/generate-string {"model" "gpt-3.5-turbo"
+                                  "messages"  [{"role" "system"
+                                                "content" (schema->system-prompt response-schema)}
+                                               {"role" "user"
+                                                "content" prompt}]
+                                  "temperature" 0.7
+                                  "max_tokens" 150})]
+
+    (-> (http/post api-url {:headers headers :body body})
+        deref ;; Dereference the future
+        :body
+        (cc/parse-string true)
+        :choices
+        first
+        :message
+        :content
+        (cc/parse-string true))))
+
+;; Example usage
+(comment
+
+    ;; https://github.com/jxnl/instructor/blob/cea534fd2280371d2778e0f043d3fe557cc7bc7e/instructor/process_response.py#L245C17-L250C83
+
+  (def User
+    [:map
+     [:name :string]
+     [:age :int]])
+
+  (llm->response "John Doe is 30 years old."
+                  User
+                  :api-key "<API-KEY>"))
